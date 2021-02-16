d1e526f0cfb3c66c0c2659799dfe77ddc001a471
NoronhaGCP <inc.691@gmail.com>
2021-02-16 09:28:43 -0300

    feat: modularize UserInterface

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    created_api_ids: string;
+    liked_api_ids: string;
+    followers: string;
+    following: string;
+    score: number;
+}
--- src/utils/searchByEmail.ts
+++ src/utils/searchByEmail.ts
@@ -1,16 +1,6 @@
 import db from '../database/connection';
 
-interface UserInterface {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    created_api_ids: string;
-    liked_api_ids: string;
-    followers: string;
-    following: string;
-    score: number;
-}
+import { UserInterface } from '../types';
 
 export default async function searchByEmail<T extends keyof UserInterface>(email: string, ...request: T[]) {
     type requestResponse = Pick<UserInterface, T>
